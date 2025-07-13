326da6c3b0b82816b1d3c2efa3454ffc71b849d3
kobayashiyabako16g <all.laisser.fair@gmail.com>
2025-07-14 01:57:50 +0900

    Update: dpp.vim init

--- config/nvim/dpp.ts
+++ config/nvim/dpp.ts
@@ -1,12 +1,35 @@
+import type {
+  ContextBuilder,
+  ExtOptions,
+  Plugin,
+  ProtocolName,
+} from "jsr:@shougo/dpp-vim@~4.5.0/types";
+import type { Protocol } from "jsr:@shougo/dpp-vim@~4.5.0/protocol";
+import type { Dpp } from "jsr:@shougo/dpp-vim@~4.5.0/dpp";
+import { mergeFtplugins } from "jsr:@shougo/dpp-vim@~4.5.0/utils";
 import {
   BaseConfig,
-  ContextBuilder,
-  Dpp,
-  Plugin,
-} from "https://deno.land/x/dpp_vim@v1.0.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/dpp_vim@v1.0.0/deps.ts";
+  type ConfigReturn,
+  type MultipleHook,
+} from "jsr:@shougo/dpp-vim@~4.5.0/config";
+import type {
+  Ext as TomlExt,
+  Params as TomlParams,
+} from "jsr:@shougo/dpp-ext-toml@~1.3.0";
+import type {
+  Ext as LazyExt,
+  LazyMakeStateResult,
+  Params as LazyParams,
+} from "jsr:@shougo/dpp-ext-lazy@~1.5.0";
+import type {
+  Ext as LocalExt,
+  Params as LocalParams,
+} from "jsr:@shougo/dpp-ext-local@~1.3.0";
+
+import type { Denops } from "jsr:@denops/std@~7.6.0";
 
 console.log("dpp.ts is loaded!");
+const BASE_PATH = "~/.config/nvim";
 
 export class Config extends BaseConfig {
   override async config(args: {
@@ -14,99 +37,142 @@ export class Config extends BaseConfig {
     contextBuilder: ContextBuilder;
     basePath: string;
     dpp: Dpp;
-  }): Promise<{
-    ftplugins: Record<string, string>;
-    hooksFiles: string[];
-    plugins: Plugin[];
-    stateLines: string[];
-  }> {
+  }): Promise<ConfigReturn> {
     args.contextBuilder.setGlobal({
       protocols: ["git"],
     });
 
-    type Toml = {
-      hooks_file?: string;
-      ftplugins?: Record<string, string>;
-      plugins?: Plugin[];
-    };
-
-    type LazyMakeStateResult = {
-      plugins: Plugin[];
-      stateLines: string[];
-    };
-
     const [context, options] = await args.contextBuilder.get(args.denops);
-    const dotfilesDir = "~/.config/nvim/";
-
-    const tomls: Toml[] = [];
-
-    const toml_files: { name: string; lazy: boolean }[] = [
-      { name: "dpp.toml", lazy: false },
-      { name: "dpp_lazy.toml", lazy: true },
-      { name: "lsp.toml", lazy: true },
-    ];
-
-    const toml_promises = toml_files.map(async (toml) => {
-      console.log(toml);
-      tomls.push(
-        (await args.dpp.extAction(
-          args.denops,
-          context,
-          options,
-          "toml",
-          "load",
-          {
-            path: await fn.expand(args.denops, dotfilesDir + toml.name),
-            options: {
-              lazy: toml.lazy,
-            },
-          },
-        )) as Toml,
-      );
-    });
-
-    await Promise.all(toml_promises);
+    const protocols = await args.denops.dispatcher.getProtocols() as Record<
+      string,
+      Protocol
+    >;
 
     const recordPlugins: Record<string, Plugin> = {};
     const ftplugins: Record<string, string> = {};
     const hooksFiles: string[] = [];
+    let multipleHooks: MultipleHook[] = [];
 
-    tomls.forEach((toml) => {
-      for (const plugin of toml.plugins) {
-        recordPlugins[plugin.name] = plugin;
-      }
+    // Load toml plugins
+    const [tomlExt, tomlOptions, tomlParams]: [
+      TomlExt | undefined,
+      ExtOptions,
+      TomlParams,
+    ] = await args.denops.dispatcher.getExt(
+      "toml",
+    ) as [TomlExt | undefined, ExtOptions, TomlParams];
+    if (tomlExt) {
+      const action = tomlExt.actions.load;
 
-      if (toml.ftplugins) {
-        for (const filetype of Object.keys(toml.ftplugins)) {
-          if (ftplugins[filetype]) {
-            ftplugins[filetype] += `\n${toml.ftplugins[filetype]}`;
-          } else {
-            ftplugins[filetype] = toml.ftplugins[filetype];
-          }
+      const tomlPromises = [
+        { path: `${BASE_PATH}/dpp.toml`, lazy: false },
+      ].map((tomlFile) =>
+        action.callback({
+          denops: args.denops,
+          context,
+          options,
+          protocols,
+          extOptions: tomlOptions,
+          extParams: tomlParams,
+          actionParams: {
+            path: tomlFile.path,
+            options: {
+              lazy: tomlFile.lazy,
+            },
+          },
+        })
+      );
+
+      const tomls = await Promise.all(tomlPromises);
+
+      // Merge toml results
+      for (const toml of tomls) {
+        for (const plugin of toml.plugins ?? []) {
+          recordPlugins[plugin.name] = plugin;
+        }
+
+        if (toml.ftplugins) {
+          mergeFtplugins(ftplugins, toml.ftplugins);
+        }
+
+        if (toml.multiple_hooks) {
+          multipleHooks = multipleHooks.concat(toml.multiple_hooks);
+        }
+
+        if (toml.hooks_file) {
+          hooksFiles.push(toml.hooks_file);
         }
       }
+    }
 
-      if (toml.hooks_file) {
-        hooksFiles.push(toml.hooks_file);
+    const [localExt, localOptions, localParams]: [
+      LocalExt | undefined,
+      ExtOptions,
+      LocalParams,
+    ] = await args.denops.dispatcher.getExt(
+      "local",
+    ) as [LocalExt | undefined, ExtOptions, LocalParams];
+    if (localExt) {
+      const action = localExt.actions.local;
+
+      const localPlugins = await action.callback({
+        denops: args.denops,
+        context,
+        options,
+        protocols,
+        extOptions: localOptions,
+        extParams: localParams,
+        actionParams: {
+          directory: `${BASE_PATH}/local/plugins`,
+          options: {
+            frozen: true,
+            merged: false,
+          },
+        },
+      });
+
+      for (const plugin of localPlugins) {
+        if (plugin.name in recordPlugins) {
+          recordPlugins[plugin.name] = Object.assign(
+            recordPlugins[plugin.name],
+            plugin,
+          );
+        } else {
+          recordPlugins[plugin.name] = plugin;
+        }
       }
-    });
+    }
 
-    const lazyResult = (await args.dpp.extAction(
-      args.denops,
-      context,
-      options,
+    const [lazyExt, lazyOptions, lazyParams]: [
+      LazyExt | undefined,
+      ExtOptions,
+      LazyParams,
+    ] = await args.denops.dispatcher.getExt(
       "lazy",
-      "makeState",
-      {
-        plugins: Object.values(recordPlugins),
-      },
-    )) as LazyMakeStateResult;
+    ) as [LazyExt | undefined, ExtOptions, LazyParams];
+    let lazyResult: LazyMakeStateResult | undefined = undefined;
+    if (lazyExt) {
+      const action = lazyExt.actions.makeState;
+
+      lazyResult = await action.callback({
+        denops: args.denops,
+        context,
+        options,
+        protocols,
+        extOptions: lazyOptions,
+        extParams: lazyParams,
+        actionParams: {
+          plugins: Object.values(recordPlugins),
+        },
+      });
+    }
 
     return {
       ftplugins,
       hooksFiles,
-      plugins: lazyResult.plugins,
-      stateLines: lazyResult.stateLines,
+      multipleHooks,
+      plugins: lazyResult?.plugins ?? [],
+      stateLines: lazyResult?.stateLines ?? [],
     };
   }
 }
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -16,68 +16,74 @@ end
 
 
 -- start prelude
-local dpp_src = vim.fn.expand("$HOME/.cache/dpp/repos/github/Shougo/dpp.vim")
+local dppBase = vim.fn.expand("~/.cache/dpp")
+local dpp_src = vim.fn.expand("~/.cache/dpp/repos/github.com/Shougo/dpp.vim")
 vim.opt.runtimepath:prepend(dpp_src)
 
 local dpp = require("dpp")
 -- end prelude
 
-local dppBase = vim.fn.expand("~/.cache/dpp")
 local dpp_config = vim.fn.expand("~/.config/nvim/dpp.ts")
--- local denops_src = vim.fn.expand("~/.cache/dpp/repos/github/vim-denops/denops.vim")
-local denops_src = vim.fn.expand("~/.nix-profile/plugin/denops.vim")
-local denops_hello = vim.fn.expand("~/.cache/dpp/repos/github/vim-denops/denops-helloworld.vim")
-local ext_toml = vim.fn.expand("~/.cache/dpp/repos/github/Shougo/dpp-ext-toml")
-local ext_lazy = vim.fn.expand("~/.cache/dpp/repos/github/Shougo/dpp-ext-lazy")
-local ext_installer = vim.fn.expand("~/.cache/dpp/repos/github/Shougo/dpp-ext-installer")
-local ext_git = vim.fn.expand("~/.cache/dpp/repos/github/Shougo/dpp-protocol-git")
+local denops_src = vim.fn.expand("~/.cache/dpp/repos/github.com/vim-denops/denops.vim")
+local denops_hello = vim.fn.expand("~/.cache/dpp/repos/github.com/vim-denops/denops-helloworld.vim")
+local ext_toml = vim.fn.expand("~/.cache/dpp/repos/github.com/Shougo/dpp-ext-toml")
+local ext_lazy = vim.fn.expand("~/.cache/dpp/repos/github.com/Shougo/dpp-ext-lazy")
+local ext_installer = vim.fn.expand("~/.cache/dpp/repos/github.com/Shougo/dpp-ext-installer")
+local ext_local = vim.fn.expand("~/.cache/dpp/repos/github.com/Shougo/dpp-ext-local")
+local ext_git = vim.fn.expand("~/.cache/dpp/repos/github.com/Shougo/dpp-protocol-git")
 
-vim.opt.runtimepath:append(ext_toml)
-vim.opt.runtimepath:append(ext_lazy)
-vim.opt.runtimepath:append(ext_git)
+-- vim.g["denops#debug"] = 1
+local ok = dpp.load_state(dppBase)
+-- vim.notify("dpp.load_state returned: " .. tostring(ok))
 vim.opt.runtimepath:prepend(denops_src)
-vim.opt.runtimepath:append(denops_hello)
+vim.opt.runtimepath:prepend(denops_hello)
+vim.opt.runtimepath:prepend(ext_installer)
+vim.opt.runtimepath:prepend(ext_local)
+vim.opt.runtimepath:prepend(ext_lazy)
+vim.opt.runtimepath:prepend(ext_toml)
+vim.opt.runtimepath:prepend(ext_git)
 
-print("dpp.load_state returned: " .. tostring(dpp.load_state(dppBase)))
+if ok then
+  -- vim.notify("dpp.load_state() is successful")
 
--- if dpp.load_state(dppBase) then
---   vim.opt.runtimepath:append(ext_installer)
-
---   vim.api.nvim_create_autocmd("User", {
---     pattern = "DenopsReady",
---     callback = function()
---       vim.notify("dpp load_state() is failed")
---       dpp.make_state(dppBase, dpp_config)
---     end,
---   })
--- end
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "DenopsReady",
+    callback = function()
+      vim.notify("dpp load_state() is failed")
+      dpp.make_state(dppBase, dpp_config)
+    end,
+  })
+end
 
 
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "Dpp:makeStatePost",
---   callback = function()
---     vim.notify("dpp make_state() is done")
---   end,
--- })
+vim.api.nvim_create_autocmd("User", {
+  pattern = "Dpp:makeStatePost",
+  callback = function()
+    -- vim.notify("dpp make_state() is done")
+  end,
+})
 
 -- ---------------------------------------------------------
 
 
--- vim.cmd("filetype indent plugin on")
--- vim.cmd("syntax on")
+vim.cmd("filetype indent plugin on")
+vim.cmd("syntax on")
 
--- vim.api.nvim_create_user_command("DppInstall", "call dpp#async_ext_action('installer', 'install')", { nargs = 0 })
--- vim.api.nvim_create_user_command("DppUpdate", "call dpp#async_ext_action('installer', 'update')", { nargs = 0 })
--- vim.api.nvim_create_user_command("DppMakestate", function(val)
---   dpp.make_state(dppBase, dpp_config)
--- end, { nargs = 0 })
+vim.api.nvim_create_user_command("DppInstall", "call dpp#async_ext_action('installer', 'install')", { nargs = 0 })
+vim.api.nvim_create_user_command("DppUpdate", "call dpp#async_ext_action('installer', 'update')", { nargs = 0 })
+vim.api.nvim_create_user_command("DppMakestate", function(val)
+  dpp.make_state(dppBase, dpp_config)
+end, { nargs = 0 })
+vim.api.nvim_create_user_command("DppClearstate", function(val)
+  dpp.clear_state()
+end, { nargs = 0 })
 
 
--- vim.opt.laststatus = 3
--- vim.opt.cursorline = true
+vim.opt.laststatus = 3
+vim.opt.cursorline = true
 
--- vim.cmd("set completeopt+=noinsert")
+vim.cmd("set completeopt+=noinsert")
 
--- vim.opt.virtualedit = "none"
+vim.opt.virtualedit = "none"
 
--- vim.opt.expandtab = true
+vim.opt.expandtab = true
